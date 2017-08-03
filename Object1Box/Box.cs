e3d0e12b06880758245691b995d18fd8d0bfd16a
RoyLWright <roylwright@gmail.com>
2017-08-03 18:01:23 -0500

    first again

---
+++ Object1Box/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object1Box
+{
+    class Box
+    {
+        public double c { get; set; }
+        public double sa;
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+        public double pricePerInch2 { get; set; }
+
+        public double Volume()
+        {
+            return x * y * z;
+        }
+        public double SurfaceArea()
+        {
+            sa = ((x * y) + (y * z) + (z * x)) * 2;
+            return sa;
+        }
+        public double Cost()
+        {
+            return (sa * c);
+        }
+
+    }
+}
---
+++ Object1Box/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object1Box
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Box b = new Box();
+            b.x = 2;
+            b.y = 3;
+            b.z = 4;
+            b.c = 10;
+            Console.WriteLine("\nThe volume of the box is: "+b.Volume());
+            Console.WriteLine("\nThe surface area of the box is: "+b.SurfaceArea());
+            Console.WriteLine("\nThe cost of the box is: "+b.Cost());
+            Console.ReadLine();
+        }
+        
+        
+    }
+}
